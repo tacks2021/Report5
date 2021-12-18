e2c667cf719af8b74b3eb060a89d3bcc201c8025
Yuuki Takusi <e215737@ie.u-ryukyu.ac.j>
2021-12-18 15:53:54 +0900

    Handling used Integer.parseInt

--- src/jp/ac/uryukyu/ie/e215737/Main.java
+++ src/jp/ac/uryukyu/ie/e215737/Main.java
@@ -1,15 +1,9 @@
 package jp.ac.uryukyu.ie.e215737;
 
 public class Main{
-    public static void main(String[] args) throws NullPointerException{
-        String str = null;
-        try{
-                System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("エラー : NullPointerException");
-            System.out.println(e.getMessage());
-        }
-        
-            
+    public static void main(String[] args) throws NullPointerException {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+                
     }
 }
