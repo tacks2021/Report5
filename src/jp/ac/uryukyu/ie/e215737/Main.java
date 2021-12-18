087b54762de1da178b4cf1a60437df237c9ca0e9
Yuuki Takusi <e215737@ie.u-ryukyu.ac.j>
2021-12-18 15:22:12 +0900

    Writing exception handling

--- src/jp/ac/uryukyu/ie/e215737/Main.java
+++ src/jp/ac/uryukyu/ie/e215737/Main.java
@@ -1,9 +1,15 @@
 package jp.ac.uryukyu.ie.e215737;
 
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NullPointerException{
         String str = null;
+        try{
+                System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("エラー : NullPointerException");
+            System.out.println(e.getMessage());
+        }
         
-            System.out.println(str.length());
+            
     }
 }
